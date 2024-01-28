f30be27dc1a9b48e716dc539738f436628ad63f7
gungun <khandelwalgungun525@gmail.com>
2024-01-28 14:29:52 +0530

    Making Login and logout

--- shop/urls.py
+++ shop/urls.py
@@ -6,7 +6,9 @@ urlpatterns=[
     path('cart/',views.cart,name='Cart'),
     path('checkout/',views.checkout,name='Checkout'),
     path("products/<int:myid>", views.prodView, name="ProdView"),
-    path('contact/',views.contact,name='ContactUs'), 
+    path('contact/',views.contact,name='ContactUs'),
+    path('login/',views.login_user,name='login'),
+    path('logout/',views.logout_user,name='logout'),
 
     path('update_item/',views.updateItem,name='update_item'),
     path('process_order/',views.processOrder,name='process_order'),
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import JsonResponse
 from .models import *
 import json
 import datetime
 from django.views.decorators.csrf import csrf_exempt
 
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 from .utilis import cookieCart ,cartData
 
@@ -100,3 +102,26 @@ def processOrder(request):
     else:
         print('User is not logged in ..')
     return JsonResponse('Payment complete',safe=False)
+
+# User authentication
+
+def login_user(request):
+    if request.method =='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,('You have been logged In!!'))
+            return redirect('index')
+        else:
+            messages.success(request,('There is an error,Try Again..'))
+            return render(request,'login.html')
+    else:
+        return render(request,'login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,('You have been logged out !'))
+    return redirect('index')
+
